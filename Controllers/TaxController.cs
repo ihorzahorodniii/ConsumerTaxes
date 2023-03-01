b4d5ac216da75af821b4baf3b683c574efc759c6
ihorzagorodnii@icloud.com <ihorzagorodnii@icloud.com>
2023-03-01 08:12:04 -0600

    Add project files.

---
+++ Controllers/TaxController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace ConsumerTaxes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxController : ControllerBase
+    {
+        protected readonly IConfiguration Configuration;
+        private DateTime datetax;
+        private string urlApi;
+        private dynamic apiResponse;
+        public TaxController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            urlApi = Configuration.GetConnectionString("API");
+        }
+
+        [HttpGet("FindTax")]
+        public async Task<IActionResult> FindTax(string Municipality, string DateTax)
+        {
+            try
+            {
+                if (!DateTime.TryParse(DateTax, out datetax))
+                    return BadRequest();
+
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.GetAsync($"{urlApi}Tax/FindTax?Municipality={Municipality}&DateTax={DateTax}"))
+                    {
+                        if (!response.IsSuccessStatusCode)
+                            return BadRequest();
+
+                        apiResponse = JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync());
+                    }
+                }
+
+                return Ok($"Tax is:{apiResponse.value}, at {Municipality} during {DateTax}");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new JsonResult(ex));
+            }
+        }
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,20 @@
+
+FROM mcr.microsoft.com/dotnet/aspnet:7.0 AS base
+WORKDIR /app
+EXPOSE 80
+
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
+WORKDIR /src
+COPY ["ConsumerTaxes.csproj", "."]
+RUN dotnet restore "./ConsumerTaxes.csproj"
+COPY . .
+WORKDIR "/src/."
+RUN dotnet build "ConsumerTaxes.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "ConsumerTaxes.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "ConsumerTaxes.dll"]
